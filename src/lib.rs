a7045e3298fd2338856af75f4b5d31b57b6ae2ee
isienmai <ben.strutt@hotmail.co.uk>
2022-01-16 16:40:39 +0000

    Added TODO: to remind me to actually parse the sources list.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use wasm_bindgen::prelude::*;
 
+mod pulse;
+
 #[wasm_bindgen]
 extern{
     pub fn alert(s: &str);
@@ -10,4 +12,8 @@ pub fn greet(name: &str) {
     alert(&format!("why HELLO THERE {}!", name));
 }
 
-mod pulse;
+#[wasm_bindgen]
+pub fn parse_sources(sources_list: &str) {
+
+    // TODO: Parse the sources list using the pulse module
+}
