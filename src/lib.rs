c52c8d19a7a68bc082c82466189c6ff94cacca7b
isienmai <ben.strutt@hotmail.co.uk>
2022-01-22 15:15:24 +0000

    Created and imported my own custom javascript function into rust.

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,20 @@ use wasm_bindgen::prelude::*;
 
 mod pulse;
 
-#[wasm_bindgen]
+#[wasm_bindgen(module = "/testing.js")]
 extern{
-    pub fn alert(s: &str);
+    pub fn custom_alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn parse_sources(sources_list: &str) {
     let sources = pulse::parse_sources(sources_list);
-    alert(&format!("Sources: {}!", sources.len()));
+    custom_alert(&format!("Sources: {}!", sources.len()));
 }
 
 #[wasm_bindgen]
 pub fn parse_sinks(sources_list: &str) {
     let sinks = pulse::parse_sinks(sources_list);
-    alert(&format!("Sinks: {}!", sinks.len()));
+    custom_alert(&format!("Sinks: {}!", sinks.len()));
 }
 
---
+++ testing.js
@@ -0,0 +1,4 @@
+
+export function custom_alert(alert_text){
+    alert(['TESTING:', alert_text].join(' '));
+}
