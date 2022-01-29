f051f265a9ee5a0bf4329ed25857d411769ca7bf
isienmai <ben.strutt@hotmail.co.uk>
2022-01-29 17:49:53 +0000

    Update frontend to reset the lists whenever the input is parsed

--- ui_control.js
+++ ui_control.js
@@ -1,7 +1,6 @@
 
 export function append_source(new_source_name, new_source_description){
     let source_list_item = document.createElement("li");
-
     source_list_item.appendChild(document.createTextNode(new_source_description));
 
     document.getElementById('sources_display').appendChild(source_list_item);
@@ -9,7 +8,6 @@ export function append_source(new_source_name, new_source_description){
 
 export function append_sink(new_sink_name, new_sink_description){
     let sink_list_item = document.createElement("li");
-
     sink_list_item.appendChild(document.createTextNode(new_sink_description));
 
     document.getElementById('sinks_display').appendChild(sink_list_item);
