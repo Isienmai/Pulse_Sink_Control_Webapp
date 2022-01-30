e2b43ae614a6b6c23aa0f24714434990e66b53d2
isienmai <ben.strutt@hotmail.co.uk>
2022-01-30 11:23:53 +0000

    Updated the rust side to have a more object oriented interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
 
 mod pulse;
+use pulse::pulse_object::source::Source;
+use pulse::pulse_object::sink::Sink;
 
 #[wasm_bindgen(module = "/ui_control.js")]
 extern{
@@ -8,19 +10,61 @@ extern{
     pub fn append_sink(name: &str, description: &str);
 }
 
+/// Struct to hold all sources/sinks/connections/module changes
 #[wasm_bindgen]
-pub fn parse_sources(sources_list: &str) {
-    let sources = pulse::parse_sources(sources_list);
-    for source in sources{
-        append_source(&source.name, &source.description);
-    }
+pub struct PulseData{
+    sources: Vec<Source>,
+    sinks: Vec<Sink>
 }
 
 #[wasm_bindgen]
-pub fn parse_sinks(sinks_list: &str) {
-    let sinks = pulse::parse_sinks(sinks_list);
-    for sink in sinks{
-        append_sink(&sink.name, &sink.description);
+impl PulseData{
+    /// Set this object's Sources to those contained in a pactl list output string
+    pub fn update_sources(&mut self, sources_list: &str){
+        self.sources = pulse::parse_sources(sources_list);
+    }
+
+    /// Set this object's Sinks to those contained in a pactl list output string
+    pub fn update_sinks(&mut self, sinks_list: &str){
+        self.sinks = pulse::parse_sinks(sinks_list);
+    }
+
+    /// Add a new Sink this object's list of sinks
+    /// This will also automatically add a monitor of the new sink to the sources list
+    pub fn add_new_sink(&mut self, new_sink_name: &str){
+        self.sinks.push(Sink{
+            name: String::from(new_sink_name), 
+            description: String::from(new_sink_name)});
+        self.sources.push(Source{
+            name: String::from(new_sink_name) + ".monitor", 
+            description: String::from(new_sink_name) + ".monitor"});
+    }
+
+    /// Get the number of Sources
+    pub fn get_sources_count(&self)->usize{
+        return self.sources.len();
+    }
+
+    /// Get the number of Sinks
+    pub fn get_sinks_count(&self)->usize{
+        return self.sources.len();
+    }
+
+    /// Get the description of a specified Source object (Specified by index)
+    /// Returns "NULL" if the specified index is invalid
+    pub fn get_source_description(&self, source_index: usize)->String{
+        return self.sources.get(source_index)
+            .and_then(|source| Some(source.description.clone()))
+            .or(Some(String::from("NULL")))
+            .unwrap();
+    }
+
+    /// Get the description of a specified Sink object (Specified by index)
+    /// Returns "NULL" if the specified index is invalid
+    pub fn get_sink_description(&self, sink_index: usize)->String{
+        return self.sinks.get(sink_index)
+            .and_then(|sink| Some(sink.description.clone()))
+            .or(Some(String::from("NULL")))
+            .unwrap();
     }
 }
-
